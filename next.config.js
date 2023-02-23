b5d9aa66751d704d63de844fc21bdd4ad12393e0
Grace Yeh <learningg@gmail.com>
2023-02-23 16:57:00 +0800

    update next.config.js for github action

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+const repo = isGithubActions
+  ? process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  : '';
+
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
+  assetPrefix: isGithubActions ? `/${repo}/` : '',
+  basePath: isGithubActions ? `/${repo}` : '',
 };
 
 module.exports = nextConfig;
